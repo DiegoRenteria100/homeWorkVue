1e9d824bca762d9b49410bec5253a5e76eeb898c
Diego:Casa <diego.renteria@sciodev.com>
2023-04-02 23:55:14 -0500

    tarea hecha

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="container">
+    <header>
+      <h1>Lista de estudiantes cursando el último año</h1>
+    </header>
+    <div class="cartas">
+      <student-details v-for="(student,idx) in students" :key="idx" :student-info="student"></student-details>
+    </div>
+    <footer></footer>
+  </div>
+</template>
+
+<script>
+import StudentDetails from './components/StudentDetails.vue';
+import axios from "axios";
+
+export default {
+  name: 'App',
+  components: {
+    StudentDetails
+  },
+  data() {
+    return {
+      students: null,
+    }
+  },
+  mounted() {
+    axios
+      .get("https://randomuser.me/api/?results=30&inc=name,email,phone,picture&noinfo")
+      .then((response) => (this.students = response.data.results));
+      console.log(this.students);
+  },
+  methods: {
+    // letterCounter(name){
+    //   return name.lenght;
+    // }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  background-color: #0b0d0f;
+  margin: 0px;
+}
+.cartas{
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: center;
+}
+header h1{ 
+  background-color: rgb(192, 231, 251);
+  margin: 0;
+}
+.container{
+  background-color: #0b0d0f;
+  margin: 0;
+  height: 100%;
+  width: 100%;
+}
+
+body{
+  background-color: #0b0d0f;
+  margin: 0;
+}
+
+</style>
---
+++ src/components/StudentDetails.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    <img :src="studentInfo.picture.large">
+    <p>{{`Name: ${studentInfo.name.first} ${studentInfo.name.last}` }}</p>
+    <div v-if="showDetails">
+      <p>{{ studentInfo.email }}</p>
+      <p>{{ studentInfo.phone }}</p>
+      <!-- <p v-if="studentInfo.age >= 18">Last year in school</p> -->
+    </div>
+    <button @click="show()">{{showDetails ? 'Hide details' : 'Show details'}}</button>
+  </div>
+</template>
+  
+<script>
+
+export default {
+  name: 'StudentDetails',
+  props: {
+    studentInfo: { type: Object, default: () => { } },
+  },
+  data() {
+    return {
+      showDetails: false,
+    }
+  },
+  methods: {
+    show(){
+      this.showDetails = !this.showDetails;
+    }
+  },
+}
+</script>
+  
+<style>
+button{
+  background-color: black;
+  color: white;
+  margin: 5px;
+}
+
+.card{
+  border: 1px solid;
+  width: 250px;
+  padding: 10px;
+  margin: 5px;
+  justify-content: center;
+  background-color: rgb(192, 231, 251);
+}
+
+</style>
+  
